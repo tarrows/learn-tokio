0ca22a95a4a54bcb75bc80756f0ad74137b3a153
c10t <c10t@fa.ke>
2020-08-03 23:52:47 +0900

    len for vec3

--- ray-tracing/src/vec3.rs
+++ ray-tracing/src/vec3.rs
@@ -29,6 +29,23 @@ impl Vec3 {
   pub fn b(self) -> f64 {
     self.2
   }
+
+  pub fn len(self) -> f64 {
+    (self.0 * self.0 + self.1 * self.1 + self.2 * self.2).sqrt()
+  }
+
+  pub fn squared_len(self) -> f64 {
+    self.0 * self.0 + self.1 * self.1 + self.2 * self.2
+  }
+
+  pub fn unit(self) -> Vec3 {
+    if self.0 == 0.0 && self.1 == 0.0 && self.2 == 0.0 {
+      Vec3(0.0, 0.0, 0.0)
+    } else {
+      let k = 1.0 / self.len();
+      Vec3(self.0 * k, self.1 * k, self.2 * k)
+    }
+  }
 }
 
 impl ops::Add for Vec3 {
@@ -51,6 +68,30 @@ impl ops::Sub for Vec3 {
       self.x() - _rhs.x(),
       self.y() - _rhs.y(),
       self.z() - _rhs.z(),
+    )
+  }
+}
+
+impl ops::Mul for Vec3 {
+  type Output = Vec3;
+
+  fn mul(self, _rhs: Vec3) -> Vec3 {
+    Vec3(
+      self.x() * _rhs.x(),
+      self.y() * _rhs.y(),
+      self.z() * _rhs.z(),
+    )
+  }
+}
+
+impl ops::Div for Vec3 {
+  type Output = Vec3;
+
+  fn div(self, _rhs: Vec3) -> Vec3 {
+    Vec3(
+      self.x() / _rhs.x(),
+      self.y() / _rhs.y(),
+      self.z() / _rhs.z(),
     )
   }
 }
